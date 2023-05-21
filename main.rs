a401627f6777625300e5d013f193adffebce7f9e
kastr <74778595+kastr0419@users.noreply.github.com>
2023-05-22 00:46:30 +0900

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,13 @@
+use std::env;
+use std::fs::File; //ファイルを扱うライブラリ
+use std::io::prelude::*; //ファイル入出力ライブラリ
+std::io::BufReader; 
+
+struct FileHeader{
+    u8:data[file_header_size];
+    
+}
+
+fn main() {
+    println!("Hello, world!");
+}
