8a22a4171ea11d4914192e66a6fe9b1f17bd6130
kastr <74778595+kastr0419@users.noreply.github.com>
2023-06-09 13:30:33 +0900

    Update main.rs

--- main.rs
+++ main.rs
@@ -4,10 +4,22 @@ use std::io::prelude::*; //ファイル入出力ライブラリ
 std::io::BufReader; 
 
 struct FileHeader{
-    filesize:u32,
+    filesize: u32,
+    filetype: String,
     offset: u32,
 }
 
 struct bitmapinfoheader{
-    
+    width: i32,
+    height: i32,
+    bitperpix: u16,
+    datasize: u32,
 }
+
+struct Color{
+    red: i32,
+    grean: i32,
+    blue: i32,
+}
+
+fn bit2integer()
